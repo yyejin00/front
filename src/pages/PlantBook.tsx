d16d9b91747d9acb0d10208d9a6b3c198937fc35
wdBear <wondong0508@gmail.com>
2025-02-20 22:10:29 +0900

    😎 식물도감 메인, 추천 식물 UI

--- src/pages/PlantBook.tsx
+++ src/pages/PlantBook.tsx
@@ -1,10 +1,56 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TextInput, StyleSheet} from 'react-native';
+import HotPlant from '../components/HotPlant';
+
 const PlantBook = () => {
   return (
     <View>
-      <Text>plantbook Screen</Text>
+      <View style={styles.container}>
+        <Text style={styles.TitleText}>
+          식 물 도 감
+        </Text>
+        <View style={styles.InputContainer}>
+          <TextInput placeholder='찾으려는 식물 이름' style={styles.TextInput} />
+        </View>
+      </View>
+      <View style={styles.HotPlant}>
+          <HotPlant />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  TitleText: {
+    color: 'green',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+
+  InputContainer: {
+    paddingTop: 30,
+    width: '80%',
+  },
+
+  TextInput: {
+    paddingLeft: 15,
+    height: 35,
+    fontSize: 11,
+    borderWidth: 0.5,
+    borderColor: '#989898',
+    borderRadius: 10,
+  },
+
+  HotPlant: {
+    position: 'absolute',
+    top: 410,
+  },
+});
+
 export default PlantBook;
