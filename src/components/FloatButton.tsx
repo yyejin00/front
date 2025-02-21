5f4af706d460dd8b5eb2c28b9320ef5544abd631
wdBear <wondong0508@gmail.com>
2025-02-21 18:09:19 +0900

    no message

--- src/components/FloatButton.tsx
+++ src/components/FloatButton.tsx
@@ -18,7 +18,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-      },
+      }, 
 
       fab: {
         position: 'absolute',
